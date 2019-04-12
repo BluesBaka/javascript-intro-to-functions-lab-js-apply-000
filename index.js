c0d8fea87daeb6953adcc2215945de589bdfbdaf
Bryna Dunnells <badunnells@gmail.com>
2019-04-12 22:33:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-   if (string === string.lowercase){
+   if (string === lowercase){
    return "I can't hear you"+"!"
  }
-    else if (string === string.uppercase){
+    else if (string === uppercase){
    return "YES INDEED"+"!"
  }
    else if (string === "I love you, Grandma.") {
